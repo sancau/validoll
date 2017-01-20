cf3f1374c853522889f7451419bd8d43aab82107
Alexander Tatchin <imcapableofanything@gmail.com>
2017-01-20 11:35:38 +0300

    feat: add new features
    add collection handler
    add invalid objects handler callback
    revised output object to save all the errors 
    added exception helpers to raise exceptions from lambdas

--- validoll.py
+++ validoll.py
@@ -1,14 +1,8 @@
 """
-VALIDOLL
+Validoll
 
-Simple yet effective minimalistic validation and data-preprocessing tool
-
+Simple yet effective minimalistic validation and data-processing tool
 Credits to github.com/sancau | 2017
-"""
-
-
-"""
-SOURCE ^^
 """
 
 
@@ -20,28 +14,40 @@ class ResolveError(Exception):
     pass
 
 
+def raise_validation_error(e):  # a shortcut to use in lambdas
+    raise ValidationError(e)
 
-def validate(data, schema, strict=False):
+
+def raise_resolve_error(e):  # a shortcut to use in lambdas
+    raise ResolveError(e)
+
+
+def validate(data, schema, strict=True):
     output = {}
+
+    validation_errors = []
+
+    if not isinstance(data, dict):
+        raise ValidationError('Given object is not of type <dict>: {}'.format(data))
 
     unknown = set(data.keys()) - set(schema.keys())
     if unknown and strict:
-        raise ValidationError(
+        validation_errors.append(
             'Found unknown fields while strict mode is on. '
             'Fields: {}'.format([i for i in unknown]))
     else:
-        output.update({key: data[key] for key in unknown}) 
+        output.update({key: data[key] for key in unknown})
 
     for k, v in schema.items():
-        if not k in data:
+        if k not in data:
             if v.get('required', None):
-                raise ValidationError('{} is required in {}'.format(k, data))
+                validation_errors.append('{} is required in {}'.format(k, data))
             else:
                 continue
-        
+
         defined_type = v.get('type', None)
         if defined_type and not isinstance(data[k], defined_type):
-            raise ValidationError(
+            validation_errors.append(
                 'Field <{}> has a wrong type. Expected {}. '.format(k, defined_type) +
                 'Got {} instead.'.format(type(data[k]))
             )
@@ -50,12 +56,45 @@ def validate(data, schema, strict=False):
             new_name = v.get('rename', k)
             resolve = v.get('resolve', lambda x: x)
             try:
-                output[new_name] = resolve(data[k])
+                if not v.get('drop', None):
+                    output[new_name] = resolve(data[k])
             except Exception as e:
                 raise ResolveError(
                     'Inner ex. while resolving <{}> ({}): {}'.format(k, type(e), e))
 
+    if validation_errors:
+        raise ValidationError(validation_errors)
+
     return output
+
+
+def validate_collection(collection, schema, invalid_handler=None, strict=True):
+    # data validation
+    valid = []
+    invalid = []
+    for obj in collection:
+        try:
+            validated = validate(obj, schema, strict)
+            valid.append(validated)
+        except ValidationError as e:
+            print(e)
+            obj['validation_errors'] = e
+            invalid.append(obj)
+        except ResolveError as e:
+            print(e)
+            obj['validation_errors'] = e
+            invalid.append(obj)
+
+    # validation results logging
+    vlen = len(valid)
+    invlen = len(invalid)
+    print('Got {} valid and normalized objects. {} objects are invalid.'.format(vlen, invlen))
+
+    if invalid_handler:
+        invalid_handler(invalid)
+
+    return valid, invalid
+
 
 
 """
